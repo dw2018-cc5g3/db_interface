95b4cc69494bb8d4fa8b751847ee996913139f84
Nikos Chan <nik0sc@users.noreply.github.com>
2018-04-18 03:40:40 +0800

    - move over to postgres - sample data for postgres

--- smartbin_create.sql
+++ smartbin_create.sql
@@ -1,34 +1,67 @@
--- fix default encoding (in case you want, like, emojis)
-ALTER DATABASE smartbin
-  CHARACTER SET = utf8mb4
-  COLLATE = utf8mb4_unicode_ci;
+-- Table: public.users
 
-CREATE TABLE IF NOT EXISTS Users (
-  id INT NOT NULL AUTO_INCREMENT,
-  can VARCHAR(16) UNIQUE NOT NULL, -- auto index
---  name VARCHAR(30)
---    NOT NULL,
-  display_name VARCHAR(50)
-    NOT NULL,
-  phone_number VARCHAR(10),
-  active BOOL NOT NULL,
-  PRIMARY KEY (id)
-);-- DEFAULT CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- DROP TABLE public.users;
 
-CREATE TABLE IF NOT EXISTS Items (
-  id INT NOT NULL AUTO_INCREMENT,
-  score INT NOT NULL,
-  mass INT NOT NULL, -- in grams
-  category SMALLINT NOT NULL,
-  deposited_by INT NOT NULL,
-  created_at DATETIME NOT NULL,
-  extra_info JSON,
-  PRIMARY KEY (id),
-  INDEX idx_deposited_by_1 (deposited_by),
-  FOREIGN KEY (deposited_by)
-    REFERENCES Users (id)
-    ON DELETE RESTRICT
-) DEFAULT CHARSET = utf8mb4;
+CREATE TABLE public.users
+(
+    id integer NOT NULL DEFAULT nextval('users_seq'::regclass),
+    can character varying(16) COLLATE pg_catalog."default" NOT NULL,
+    display_name character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    phone_number character varying(10) COLLATE pg_catalog."default",
+    active boolean NOT NULL,
+    name character varying(30) COLLATE pg_catalog."default" DEFAULT NULL::character varying,
+    CONSTRAINT users_pkey PRIMARY KEY (id),
+    CONSTRAINT users_can_key UNIQUE (can)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
 
--- to migrate the db to include creation time column do this:
--- ALTER TABLE Items ADD COLUMN created_at DATETIME NOT NULL;
+ALTER TABLE public.users
+    OWNER to postgres;
+
+-- Table: public.items
+
+-- DROP TABLE public.items;
+
+CREATE TABLE public.items
+(
+    id integer NOT NULL DEFAULT nextval('items_seq'::regclass),
+    score integer NOT NULL,
+    mass integer NOT NULL,
+    category smallint NOT NULL,
+    deposited_by integer NOT NULL,
+    created_at timestamp(0) without time zone NOT NULL,
+    extra_info json,
+    CONSTRAINT items_pkey PRIMARY KEY (id),
+    CONSTRAINT items_deposited_by_fkey FOREIGN KEY (deposited_by)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE RESTRICT
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.items
+    OWNER to postgres;
+
+-- Index: idx_created_at_1
+
+-- DROP INDEX public.idx_created_at_1;
+
+CREATE INDEX idx_created_at_1
+    ON public.items USING btree
+    (created_at)
+    TABLESPACE pg_default;
+
+-- Index: idx_deposited_by_1
+
+-- DROP INDEX public.idx_deposited_by_1;
+
+CREATE INDEX idx_deposited_by_1
+    ON public.items USING btree
+    (deposited_by)
+    TABLESPACE pg_default;
